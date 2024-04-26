88f6e74b3c6d87f795a6e6ad2b34982f389f777c
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-04-26 15:42:00 +0300

    feat: added users/ url and included train_api

---
+++ train_api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import UserViewSet
+
+
+urlpatterns = [
+    path("users/", UserViewSet.as_view(
+        {
+            "get": "list",
+            "post": "create",
+        }
+    ), name="users"),
+]
+
+app_name = "train_api"

a12a95f6b592e2bd8e9d80e59182a74f3ed90342
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-05-08 15:50:35 +0300

    feat: added upload_image action to TrainViewSet

--- train_api/views.py
+++ train_api/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from train_api.models import Crew, Station, TrainType, Train, Route, Journey, Order, Ticket
 from train_api.serializers import (
@@ -15,7 +17,11 @@ from train_api.serializers import (
     JourneyListSerializer,
     JourneyDetailSerializer,
     OrderListSerializer,
-    OrderDetailSerializer, TicketListSerializer, TicketDetailSerializer, JourneyCreateSerializer,
+    OrderDetailSerializer,
+    TicketListSerializer,
+    TicketDetailSerializer,
+    JourneyCreateSerializer,
+    TrainImageSerializer,
 )
 
 
@@ -50,8 +56,22 @@ class TrainViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == "list":
             return TrainListSerializer
+        elif self.action == "upload_image":
+            return TrainImageSerializer
         else:
             return TrainDetailSerializer
+
+    @action(methods=["POST"], detail=True, url_path="upload-image")
+    def upload_image(self, request, pk=None):
+        train = self.get_object()
+        serializer = self.get_serializer(train, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RouteViewSet(viewsets.ModelViewSet):
